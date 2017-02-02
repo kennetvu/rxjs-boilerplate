b16d5150658259c1343e4b6e6dcdcc470dc4f9fc
Kennet Vuong <kennet.vuong@knowit.no>
2017-02-02 22:48:12 +0100

    Made my own observerable with delay

--- src/index.js
+++ src/index.js
@@ -4,19 +4,36 @@ import Rx from 'rxjs/Rx';
 const app = document.getElementById("app");
 
 
-const fiveSeconds$ = Rx.Observable.interval(1000).take(5);
+const fiveSeconds$ = Rx.Observable.create((observer) => {
+  observer.next(2);
+  setTimeout(() => {
+    observer.next(2);
+  }, 1000);
+  setTimeout(() => {
+    observer.next(10);
+
+  }, 2000);
+  setTimeout(() => {
+    observer.next(3);
+    observer.complete();
+  }, 3000);
+});
+
+
+
+
 
 fiveSeconds$.subscribe({
   next: (val) => {
     console.log(val);
-    app.innerHTML+= val+'<br />';
+    app.innerHTML += val + '<br />';
   },
   err: () => {
 
   },
   complete: () => {
     const complete = 'COMPLETE!!!';
-    app.innerHTML+= `${complete}`;
+    app.innerHTML += `${complete}`;
   }
 
 });
