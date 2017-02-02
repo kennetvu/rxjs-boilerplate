2abae984cd79bb7a5ad5efe744a5938002bea647
Kennet Vuong <kennet.vuong@knowit.no>
2017-02-02 22:44:29 +0100

    Basic observable with interval and take

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 
 import Rx from 'rxjs/Rx';
 
-Rx.Observable.of(1,2,3).forEach((value)=> {
-  console.log(value);
+const app = document.getElementById("app");
+
+
+const fiveSeconds$ = Rx.Observable.interval(1000).take(5);
+
+fiveSeconds$.subscribe({
+  next: (val) => {
+    console.log(val);
+    app.innerHTML+= val+'<br />';
+  },
+  err: () => {
+
+  },
+  complete: () => {
+    const complete = 'COMPLETE!!!';
+    app.innerHTML+= `${complete}`;
+  }
+
 });
 
-console.log("its works!!");
